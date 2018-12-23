f9a2cd01d7e00dbbbfda1d8372c486dfbcc82db6
elsid <elsid.mail@gmail.com>
2018-12-24 00:54:44 +0300

    Limit simulations by 3 iterations

--- src/optimal_action.rs
+++ src/optimal_action.rs
@@ -113,7 +113,7 @@ impl Robot {
         next_action_id += 1;
         let steps = [1, 3, 4, 8];
         let mut iterations = 0;
-        while (iterations < 5 || optimal_action.id == 0) && global_simulator.current_time() + near_time_interval < simulation_time_depth {
+        while (iterations < 3 || optimal_action.id == 0) && global_simulator.current_time() + near_time_interval < simulation_time_depth {
             log!(world.game.current_tick, "[{}]  try time point {} {}", self.id, global_simulator.current_micro_tick(), global_simulator.current_time());
             let ball_y = global_simulator.ball().base().y;
             let ball_radius = global_simulator.ball().radius();
