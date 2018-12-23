0e18371941a0e477bd2e54819f37fb910a5000ef
elsid <elsid.mail@gmail.com>
2018-12-24 00:31:08 +0300

    Remove useless

--- src/optimal_action.rs
+++ src/optimal_action.rs
@@ -237,12 +237,7 @@ impl Robot {
                 render.add_with_tag(Tag::RobotId(self.id), Object::line(prev_robot.position, next_robot.position, 1.0, get_robot_color(i, prev.robots.len())));
             }
         }
-//        render.add(Object::line(self.position(), self.position() + optimal_action.action.target_velocity() * 100.0, 2.0, VELOCITY));
-//        render.add(Object::sphere(optimal_action.local_simulator_before_jump.me().position(), optimal_action.local_simulator_before_jump.me().radius(), OPTIMAL_ME_POSITION));
-//        render.add(Object::sphere(optimal_action.local_simulator_after_jump.me().position(), optimal_action.local_simulator_after_jump.me().radius(), OPTIMAL_ME_POSITION));
-//        render.add(Object::sphere(optimal_action.local_simulator_end.ball().position(), optimal_action.local_simulator_end.ball().radius(), OPTIMAL_BALL_POSITION));
         render.add_with_tag(Tag::RobotId(self.id), Object::text(format!("robot: {}\n  position: {:?}\n  target_speed: {}\n  velocity.norm(): {}", self.id, self.position(), optimal_action.action.target_velocity().norm(), self.velocity().norm())));
-//        log!(world.game.current_tick, "<{}> optimal action", optimal_action.id);
         optimal_action
     }
 }
