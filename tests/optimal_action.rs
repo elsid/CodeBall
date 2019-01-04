35fd4456cbb4562e303bb02c0b77bae6709e8acd
elsid <elsid.mail@gmail.com>
2019-01-04 15:47:29 +0300

    Add test for optimal action far jump

--- tests/optimal_action.rs
+++ tests/optimal_action.rs
@@ -13,3 +13,37 @@ fn test_robot_get_optimal_action() {
     assert_eq!(result.action.jump_speed, 0.0);
     assert_eq!(result.score, 1079);
 }
+
+#[test]
+fn test_robot_get_optimal_action_far_jump() {
+    use my_strategy::my_strategy::world::World;
+    use my_strategy::my_strategy::vec3::Vec3;
+    use my_strategy::my_strategy::render::Render;
+    use my_strategy::my_strategy::random::{XorShiftRng, SeedableRng};
+    use my_strategy::examples::example_world;
+
+    let mut world = example_world();
+    let mut rng = XorShiftRng::from_seed([
+        1662648909,
+        2447818268,
+        201539282,
+        3684906436,
+    ]);
+    let mut render = Render::new();
+
+    world.me.id = 2;
+    world.me.set_position(Vec3::new(-5.838617159216834, 1.0, -10.508900380791133));
+    world.me.set_velocity(Vec3::new(16.91322406429886, 0.0, 24.77759529887104));
+    let me = world.me.clone();
+    world.game.robots.iter_mut()
+        .find(|v| v.id == 2)
+        .map(|v| *v = me);
+    world.game.ball.y = 5.233161866399729;
+    world.game.ball.velocity_y = -12.500000000000554;
+
+    let result = world.me.get_optimal_action(&world, &mut rng, &mut render);
+
+    assert_eq!(result.action.target_velocity(), Vec3::new(16.913352070257815, 0.0, 24.777782825497237));
+    assert_eq!(result.action.jump_speed, 15.0);
+    assert_eq!(result.score, 1256);
+}
