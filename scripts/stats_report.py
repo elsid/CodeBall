6f0f28c620561123722799d1f245dcec3440d665
elsid <elsid.mail@gmail.com>
2019-01-21 11:55:53 +0300

    Show orders histogram

--- scripts/stats_report.py
+++ scripts/stats_report.py
@@ -6,6 +6,8 @@ import statistics
 import numpy
 import matplotlib.pyplot
 
+from collections import Counter
+
 
 def main():
     raw = [json.loads(v) for v in sys.stdin]
@@ -16,19 +18,30 @@ def main():
     values['jump_simulation'] = [v for v in values['jump_simulation']]
     row('', 'n', 'sum', 'q95', 'min', 'max', 'mean', 'median', 'stdev')
     for k, v in values.items():
-        if not v or not isinstance(v[0], (int, float)):
-            continue
-        row(
-            k,
-            len(v),
-            sum(v),
-            numpy.quantile(v, 0.95) if v else float('NaN'),
-            min(v) if v else float('NaN'),
-            max(v) if v else float('NaN'),
-            statistics.mean(v) if v else float('NaN'),
-            statistics.median(v) if v else float('NaN'),
-            statistics.stdev(v) if v else float('NaN'),
-        )
+        if not v:
+            row(k, len(v))
+        elif isinstance(v[0], (int, float)):
+            row(
+                k,
+                len(v),
+                sum(v),
+                numpy.quantile(v, 0.95) if v else float('NaN'),
+                min(v) if v else float('NaN'),
+                max(v) if v else float('NaN'),
+                statistics.mean(v) if v else float('NaN'),
+                statistics.median(v) if v else float('NaN'),
+                statistics.stdev(v) if v else float('NaN'),
+            )
+        elif isinstance(v[0], str):
+            count = Counter(v)
+            row(
+                k,
+                len(count),
+                '-',
+                '-',
+                '%s (%s)' % min(count.items(), key=lambda v: v[1]),
+                '%s (%s)' % max(count.items(), key=lambda v: v[1]),
+            )
     print()
     if result:
         row(*sorted(result[0][0].keys()))
@@ -36,21 +49,31 @@ def main():
             row(*[player[k] for k in sorted(result[0][0].keys())])
     print()
     for k, v in values.items():
-        if not v or not isinstance(v[0], (int, float)):
+        if not v:
             continue
-        fig, ax = matplotlib.pyplot.subplots()
-        fig.canvas.set_window_title(k)
-        if v and k in ('iteration', 'step', 'total_iterations'):
-            ax.hist(v, bins=numpy.arange(min(v), max(v) + 1, 1))
-            ax.set_xticks(numpy.arange(min(v), max(v) + 1, 1))
-        else:
-            ax.hist(v, bins='auto')
-        ax.grid(True)
+        elif isinstance(v[0], (int, float)):
+            fig, ax = matplotlib.pyplot.subplots()
+            fig.canvas.set_window_title(k)
+            if v and k in ('iteration', 'step', 'total_iterations'):
+                ax.hist(v, bins=numpy.arange(min(v), max(v) + 1, 1))
+                ax.set_xticks(numpy.arange(min(v), max(v) + 1, 1))
+            else:
+                ax.hist(v, bins='auto')
+            ax.grid(True)
+        elif isinstance(v[0], str):
+            count = sorted(Counter(v).items(), key=lambda v: -v[1])
+            fig, ax = matplotlib.pyplot.subplots()
+            fig.canvas.set_window_title(k)
+            x_coordinates = numpy.arange(len(count))
+            ax.bar(x_coordinates, [v[1] for v in count], align='center')
+            ax.xaxis.set_major_locator(matplotlib.pyplot.FixedLocator(x_coordinates))
+            ax.xaxis.set_major_formatter(matplotlib.pyplot.FixedFormatter(['%s (%s)' % (v[0], v[1]) for v in count]))
+            ax.grid(True)
     matplotlib.pyplot.show()
 
 
 def row(*args):
-    print(('{:>20}' * len(args)).format(*args))
+    print(('{:>25}' * len(args)).format(*args))
 
 
 if __name__ == '__main__':
