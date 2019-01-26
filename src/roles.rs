1934dd7a8555116eadb8bec3b6c8067b65da0a55
elsid <elsid.mail@gmail.com>
2019-01-27 02:56:31 +0300

    Set goalkeeper max z to nitro pack z for more than 2x2 games

--- src/roles.rs
+++ src/roles.rs
@@ -141,8 +141,12 @@ impl Goalkeeper {
     }
 
     pub fn max_z(world: &World) -> f64 {
-        (-world.rules.arena.depth / 2.0 + world.rules.arena.corner_radius + world.rules.BALL_RADIUS + 10.0)
-            .max(-world.rules.NITRO_PACK_Z)
+        if world.game.robots.len() > 4 {
+            -world.rules.NITRO_PACK_Z
+        } else {
+            (-world.rules.arena.depth / 2.0 + world.rules.arena.corner_radius + world.rules.BALL_RADIUS + 10.0)
+                .max(-world.rules.NITRO_PACK_Z)
+        }
     }
 
     pub fn can_quit(&self, world: &World) -> bool {
