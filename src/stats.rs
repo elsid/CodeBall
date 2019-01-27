4c2953b7c7b7dd71d925aecacb550a0cbc677a86
elsid <elsid.mail@gmail.com>
2019-01-27 03:38:24 +0300

    Add game micro ticks limit to stats

--- src/stats.rs
+++ src/stats.rs
@@ -17,6 +17,7 @@ pub struct Stats {
     pub action_score: i32,
     pub play_micro_ticks: i32,
     pub game_micro_ticks: usize,
+    pub game_micro_ticks_limit: usize,
     pub current_step: usize,
     pub reached_game_limit: bool,
     pub reached_play_limit: bool,
