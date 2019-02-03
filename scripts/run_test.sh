c0e9d3d404239bb175e0cd9c491c822b5b7f1a34
elsid <elsid.mail@gmail.com>
2019-02-03 15:42:46 +0300

    Support custom bin in test script, use env

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,34 +2,45 @@
 
 ID=${1}
 OTHER=${2}
-NITRO=${3}
-TEAM_SIZE=${4}
-PORTS_SHIFT=${5}
+PREFIX=
 SUFFIX=
 
 if ! [[ "${OTHER}" ]] || ! [[ "${ID}" ]]; then
-    echo "other is not set, usage: ${0} <id> <other> [<nitro> [<team_size> [ports_shift]]]"
+    echo "other is not set, usage: ${0} <id> <other>"
     exit 1
 fi
 
+if [[ ${DEBUG} ]]; then
+    PREFIX=debug_
+fi
+
 if ! [[ "${NITRO}" ]]; then
-    NITRO=false
+    NITRO=true
 else
     SUFFIX=_nitro
 fi
 
 if ! [[ "${TEAM_SIZE}" ]]; then
-    TEAM_SIZE=2
+    TEAM_SIZE=3
 fi
 
-SUFFIX=${SUFFIX}_${TEAM_SIZE}
+if ! [[ "${DURATION}" ]]; then
+    DURATION=18000
+fi
+
+SUFFIX=${SUFFIX}_${TEAM_SIZE}_${DURATION}
 
 if ! [[ "${PORTS_SHIFT}" ]]; then
     PORTS_SHIFT=0
 fi
 
-VERSION=$(git rev-parse --short HEAD)
-LOG_DIR=${PWD}/log/${OTHER}_vs_${VERSION}${SUFFIX}
+if [[ "${USE_BIN}" ]]; then
+    VERSION=$(echo ${USE_BIN} | sed 's;.*/;;')
+else
+    VERSION=$(git rev-parse --short HEAD)
+fi
+
+LOG_DIR=${PWD}/log/${PREFIX}${OTHER}_vs_${VERSION}${SUFFIX}
 RESULT=${LOG_DIR}/result.${ID}.txt
 
 mkdir -p ${LOG_DIR}
@@ -37,17 +48,20 @@ mkdir -p ${LOG_DIR}
 PLAYER_1_PORT=$((31011 + ${PORTS_SHIFT}))
 PLAYER_2_PORT=$((31012 + ${PORTS_SHIFT}))
 
-cargo build --release
+if ! [[ "${USE_BIN}" ]]; then
+    cargo build --release
+fi
 
 local_runner/codeball2018 \
     --team-size ${TEAM_SIZE} \
+    --duration ${DURATION} \
     --start-paused \
     --no-countdown \
     --results-file ${RESULT} \
     --noshow \
     --nitro ${NITRO} \
     --p1-name ${OTHER} \
-    --p2-name c \
+    --p2-name ${VERSION} \
     --p1 tcp-${PLAYER_1_PORT} \
     --p2 tcp-${PLAYER_2_PORT} &
 LOCAL_RUNNER_PID=$!
@@ -59,7 +73,11 @@ PLAYER_1_PID=$!
 trap "kill -9 ${LOCAL_RUNNER_PID} ${PLAYER_1_PID}" SIGHUP SIGINT SIGTERM
 
 sleep 1
-/usr/bin/time -v target/release/my-strategy 127.0.0.1 ${PLAYER_2_PORT} 0000000000000000
+if [[ "${USE_BIN}" ]]; then
+    /usr/bin/time -v ${USE_BIN} 127.0.0.1 ${PLAYER_2_PORT} 0000000000000000
+else
+    /usr/bin/time -v target/release/my-strategy 127.0.0.1 ${PLAYER_2_PORT} 0000000000000000
+fi
 cat ${RESULT}
 
 wait
