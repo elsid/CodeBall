d2985e63058c00e0a0f7961d5b37b975a2809052
elsid <elsid.mail@gmail.com>
2019-02-05 11:30:49 +0300

    Simplify tests

--- tests/my_strategy_impl.rs
+++ tests/my_strategy_impl.rs
@@ -56,11 +56,9 @@ fn test_two_robots_first_action_to_go_for_ball() {
 
 #[test]
 fn test_two_robots_first_ball_kick_until_goal() {
-    use my_strategy::model::Ball;
     use my_strategy::examples::{GameType, example_world, example_rng};
-    use my_strategy::my_strategy::simulator::{Simulator, RobotCollisionType, Solid};
+    use my_strategy::my_strategy::simulator::Simulator;
     use my_strategy::my_strategy::my_strategy_impl::MyStrategyImpl;
-    use my_strategy::my_strategy::entity::Entity;
 
     let world = example_world(GameType::TwoRobots);
     let mut rng = example_rng(&world.rules);
@@ -72,31 +70,6 @@ fn test_two_robots_first_ball_kick_until_goal() {
     );
 
     simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().position().y() - simulator.me().radius() < 1e-3
-            && simulator.current_tick() < 150
-    });
-
-    assert_eq!(simulator.me().position().y(), 1.2931412499999937);
-    assert_eq!(simulator.current_tick(), 38);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().collision_type() == RobotCollisionType::None
-            && simulator.current_tick() < 150
-    });
-
-    assert_eq!(simulator.ball().base(), &Ball {
-        x: -0.0011459797062779416,
-        y: 3.535510692478625,
-        z: 0.5468419748712248,
-        velocity_x: -0.07374469478481722,
-        velocity_y: 17.1299179029137,
-        velocity_z: 35.18971087488389,
-        radius: 2.0,
-    });
-    assert_eq!(simulator.me().action().jump_speed, 15.0);
-    assert_eq!(simulator.current_tick(), 45);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
         simulator.score() == 0 && simulator.current_tick() < 150
     });
 
@@ -106,11 +79,9 @@ fn test_two_robots_first_ball_kick_until_goal() {
 
 #[test]
 fn test_two_robots_with_nitro_first_ball_kick_until_goal() {
-    use my_strategy::model::Ball;
     use my_strategy::examples::{GameType, example_world, example_rng};
-    use my_strategy::my_strategy::simulator::{Simulator, RobotCollisionType, Solid};
+    use my_strategy::my_strategy::simulator::Simulator;
     use my_strategy::my_strategy::my_strategy_impl::MyStrategyImpl;
-    use my_strategy::my_strategy::entity::Entity;
 
     let world = example_world(GameType::TwoRobotsWithNitro);
     let mut rng = example_rng(&world.rules);
@@ -120,31 +91,6 @@ fn test_two_robots_with_nitro_first_ball_kick_until_goal() {
         &simulator.rules(),
         &simulator.game(),
     );
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().position().y() - simulator.me().radius() < 1e-3
-            && simulator.current_tick() < 150
-    });
-
-    assert_eq!(simulator.me().position().y(), 1.293384565284894);
-    assert_eq!(simulator.current_tick(), 38);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().collision_type() == RobotCollisionType::None
-            && simulator.current_tick() < 150
-    });
-
-    assert_eq!(simulator.ball().base(), &Ball {
-        x: 0.010140994996958893,
-        y: 3.313079288501532,
-        z: 0.26032655549236033,
-        velocity_x: 1.5435199991666337,
-        velocity_y: 18.708634435629627,
-        velocity_z: 39.623256380376844,
-        radius: 2.0,
-    });
-    assert_eq!(simulator.me().action().jump_speed, 15.0);
-    assert_eq!(simulator.current_tick(), 44);
 
     simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
         simulator.score() == 0 && simulator.current_tick() < 150
@@ -157,9 +103,8 @@ fn test_two_robots_with_nitro_first_ball_kick_until_goal() {
 #[test]
 fn test_two_robots_with_nitro_goalkeeper_should_catch_but_cant() {
     use my_strategy::examples::{GameType, example_world, example_rng};
-    use my_strategy::my_strategy::simulator::{Simulator, RobotCollisionType, Solid};
+    use my_strategy::my_strategy::simulator::Simulator;
     use my_strategy::my_strategy::my_strategy_impl::MyStrategyImpl;
-    use my_strategy::my_strategy::entity::Entity;
     use my_strategy::my_strategy::vec3::Vec3;
 
     let mut world = example_world(GameType::TwoRobotsWithNitro);
@@ -182,33 +127,18 @@ fn test_two_robots_with_nitro_goalkeeper_should_catch_but_cant() {
     );
 
     simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().position().y() - simulator.me().radius() < 1e-3
-            && simulator.current_tick() < 100
-    });
-
-    assert_eq!(simulator.current_tick(), 47);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().collision_type() == RobotCollisionType::None
-            && simulator.current_tick() < 100
-    });
-
-    assert_eq!(simulator.current_tick(), 63);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
         simulator.score() == 0 && simulator.current_tick() < 100
     });
 
     assert_eq!(simulator.score(), -1);
-    assert_eq!(simulator.current_tick(), 63);
+    assert_eq!(simulator.current_tick(), 57);
 }
 
 #[test]
 fn test_two_robots_with_nitro_goalkeeper_should_catch() {
     use my_strategy::examples::{GameType, example_world, example_rng};
-    use my_strategy::my_strategy::simulator::{Simulator, RobotCollisionType, Solid};
+    use my_strategy::my_strategy::simulator::Simulator;
     use my_strategy::my_strategy::my_strategy_impl::MyStrategyImpl;
-    use my_strategy::my_strategy::entity::Entity;
     use my_strategy::my_strategy::vec3::Vec3;
 
     let mut world = example_world(GameType::TwoRobotsWithNitro);
@@ -231,20 +161,6 @@ fn test_two_robots_with_nitro_goalkeeper_should_catch() {
     );
 
     simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().position().y() - simulator.me().radius() < 1e-3
-            && simulator.current_tick() < 100
-    });
-
-    assert_eq!(simulator.current_tick(), 29);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().collision_type() == RobotCollisionType::None
-            && simulator.current_tick() < 100
-    });
-
-    assert_eq!(simulator.current_tick(), 54);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
         simulator.score() == 0 && simulator.current_tick() < 100
     });
 
@@ -254,11 +170,9 @@ fn test_two_robots_with_nitro_goalkeeper_should_catch() {
 
 #[test]
 fn test_three_robots_with_nitro_first_ball_kick_until_goal() {
-    use my_strategy::model::Ball;
     use my_strategy::examples::{GameType, example_world, example_rng};
-    use my_strategy::my_strategy::simulator::{Simulator, RobotCollisionType, Solid};
+    use my_strategy::my_strategy::simulator::Simulator;
     use my_strategy::my_strategy::my_strategy_impl::MyStrategyImpl;
-    use my_strategy::my_strategy::entity::Entity;
 
     let world = example_world(GameType::ThreeRobotsWithNitro);
     let mut rng = example_rng(&world.rules);
@@ -268,31 +182,6 @@ fn test_three_robots_with_nitro_first_ball_kick_until_goal() {
         &simulator.rules(),
         &simulator.game(),
     );
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().position().y() - simulator.me().radius() < 1e-3
-            && simulator.current_tick() < 150
-    });
-
-    assert_eq!(simulator.me().position().y(), 1.2933846022005453);
-    assert_eq!(simulator.current_tick(), 38);
-
-    simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
-        simulator.me().collision_type() == RobotCollisionType::None
-            && simulator.current_tick() < 150
-    });
-
-    assert_eq!(simulator.ball().base(), &Ball {
-        x: 0.008630768289976823,
-        y: 3.307333838427491,
-        z: 0.23468462876435003,
-        velocity_x: 1.3843278763265943,
-        velocity_y: 18.265538210817308,
-        velocity_z: 37.642126729452535,
-        radius: 2.0,
-    });
-    assert_eq!(simulator.me().action().jump_speed, 15.0);
-    assert_eq!(simulator.current_tick(), 44);
 
     simulate_while(&mut my_strategy, &mut simulator, &mut rng, |simulator| {
         simulator.score() == 0 && simulator.current_tick() < 150
